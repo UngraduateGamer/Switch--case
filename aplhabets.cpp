f9feef612e48e267c8184af0ee755b5653f69520
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-08-02 12:31:09 +0530

    Create aplhabets.cpp

---
+++ aplhabets.cpp
@@ -0,0 +1,98 @@
+# include<iostream>
+using namespace std;
+int main()
+{	char ch;
+	cout<<"Enter Alphabet: (a-z) : ";
+	cin>>ch;
+	switch(tolower(ch))
+	{
+		case'a':
+			cout<<"Apple";
+			break;
+		case'b':
+			cout<<"Ball";
+			break;
+		case 'c':
+			cout<<"Cat";
+			break;
+		case 'd':
+			cout<<"Dog";
+			break;
+		case 'e':
+			cout<<"Elephant";
+			break;
+		case 'f':
+			cout<<"Fish";
+			break;
+		case 'g':
+			cout<<"Goat";
+			break;
+		case 'h':
+			cout<<"Horse";
+			break;
+		case 'i':
+			cout<<"Ink";
+			break;
+		case 'j':
+			cout<<"Elephant";
+			break;
+		case 'k':
+			cout<<"King";
+			break;
+		case 'l':
+			cout<<"Lion";
+			break;
+		case 'm':
+			cout<<"monkey";
+			break;
+		case 'n':
+			cout<<"Neighbour";
+			break;
+		case 'o':
+			cout<<"Outstanding";
+			break;
+		case 'p':
+			cout<<"Prince";
+			break;
+		case 'q':
+			cout<<"Queen";
+			break;
+		case 'r':
+			cout<<"Rahul";
+			break;
+		case 's':
+			cout<<"Sour";
+			break;
+		case 't':
+			cout<<"Time";
+			break;
+		case 'u':
+			cout<<"Uncle";
+			break;
+		case 'v':
+			cout<<"Venom";
+			break;
+		case 'w':
+			cout<<"Watch";
+			break;
+		case 'x':
+			cout<<"X-Ray";
+			break;
+		case 'y':
+			cout<<"Yamuna";
+			break;
+		case 'z':
+			cout<<"Zebra";
+			break;
+			
+		default:
+			cout<<"You Enter Wrong Input";
+			break;
+	}
+	
+
+	
+		
+	
+	return 0;
+}
